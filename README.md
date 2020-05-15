56e5be5db5348c25e667cd9a0ba9c45deda0b44a
Colin Hobday <chobday@sg.ibm.com>
2020-05-15 17:56:49 +0800

    full application

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 // mongoose 3 uses bson@0.4.23 which has a ciritical issue with BSON serialization
+
+db.policies.insertOne({
+    policyNumber: "12",
+    firstName: "John",
+    lastName: "Tan",
+    validDate: new Date("2018-01-01"),
+})
+
+Insert claim
+{
+	"description": "test2",
+	"policyNumber": "12",
+	"issues": [
+		{"title": "tissue", "description": "tissue"}
+	]
+}
--- src/handlers.js
+++ src/handlers.js
@@ -1,4 +1,4 @@
-// var PolicyHandler = require("./handlers/policyHandler");
+var PolicyHandler = require("./handlers/policyHandler");
 var ClaimHandler = require("./handlers/claimHandler");
 
 function setupHandlers(router) {
@@ -6,15 +6,15 @@ function setupHandlers(router) {
 		response.json({ message: "Welcome to the API" });
 	});
 
-	// router.get("/policies", function(request, response) {
-	// 	PolicyHandler.getAll(request, response);
-	// });
-	// router.get("/policies/:policyNumber", function(request, response) {
-	// 	PolicyHandler.get(request, response);
-	// });
-	// router.post("/policies/search", function(request, response) {
-	// 	PolicyHandler.search(request, response);
-	// });
+	router.get("/policies", function(request, response) {
+		PolicyHandler.getAll(request, response);
+	});
+	router.get("/policies/:policyNumber", function(request, response) {
+		PolicyHandler.get(request, response);
+	});
+	router.post("/policies/search", function(request, response) {
+		PolicyHandler.search(request, response);
+	});
 	
 	router.get("/claims", function(request, response) {
 		ClaimHandler.getAll(request, response);
@@ -22,9 +22,9 @@ function setupHandlers(router) {
 	// router.get("/claims/:claimNumber", function(request, response) {
 	// 	ClaimHandler.get(request, response);
 	// });
-	// router.post("/claims", function(request, response) {
-	// 	ClaimHandler.insert(request, response);
-	// });
+	router.post("/claims", function(request, response) {
+		ClaimHandler.insert(request, response);
+	});
 	// router.post("/claims/search", function(request, response) {
 	// 	ClaimHandler.search(request, response);
 	// });
